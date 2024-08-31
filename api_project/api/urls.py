49713160f6ebbeab05dcd728af697edff1b8d10e
Karabo Phooko <Kmphooko@gmail.com>
2024-09-01 00:15:22 +0200

    updated urls.py to include default router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -6,3 +6,16 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+
+# api/urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -10,5 +10,16 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    # api/views.py
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
 
 # Create your views here.
