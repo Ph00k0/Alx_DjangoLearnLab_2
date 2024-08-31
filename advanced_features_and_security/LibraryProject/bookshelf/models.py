196420feaabb9ede0488d0f6290236627e9fe95c
Karabo Phooko <Kmphooko@gmail.com>
2024-08-31 22:45:16 +0200

    updated models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -48,3 +48,17 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    pages = models.IntegerField()
+    cover = models.ImageField(upload_to='covers/')
+    language = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
