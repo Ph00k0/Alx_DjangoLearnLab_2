924ed9f4d5f78fccf8c3639164aec951144e99c3
Karabo Phooko <Kmphooko@gmail.com>
2024-08-31 23:13:16 +0200

    Add ExampleForm to forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -10,3 +10,9 @@ class CustomUserForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'date_of_birth', 'profile_photo']
+
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label='Name')
+    email = forms.EmailField(required=True, label='Email')
+    message = forms.CharField(widget=forms.Textarea, required=True, label='Message')
